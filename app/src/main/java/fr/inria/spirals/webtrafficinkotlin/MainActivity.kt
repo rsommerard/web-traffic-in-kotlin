d64941dbfd7f5056ceec1e0692ee58d4cdfbc777
Romain SOMMERARD <rsommerard@gmail.com>
2017-10-27 12:54:05 +0200

    Add tcp and udp local testing protocol.

--- app/src/main/java/fr/inria/spirals/webtrafficinkotlin/MainActivity.kt
+++ app/src/main/java/fr/inria/spirals/webtrafficinkotlin/MainActivity.kt
@@ -7,11 +7,17 @@ import android.widget.Button
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
-import java.net.URL
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.PrintWriter
+import java.net.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private val TAG = MainActivity::class.simpleName
+
+    private val okHttpClient = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +27,17 @@ class MainActivity : AppCompatActivity() {
         httpRequestButton.setOnClickListener {
             doAsync {
                 try {
-                    val result = URL("http://httpbin.org/html").readText()
-                    uiThread { toast("HTTP OK") }
-                    Log.d(TAG, "result=$result")
+                    val request = Request.Builder().url("http://httpbin.org/html").build()
+
+                    val response = okHttpClient.newCall(request).execute()
+
+                    if (response.isSuccessful) {
+                        uiThread { toast("HTTP OK") }
+                        Log.d(TAG, "result=${response.body()?.string()}")
+                    } else {
+                        uiThread { toast("HTTP Failed") }
+                        Log.w(TAG, "result=${response.message()}")
+                    }
                 } catch (e: Exception) {
                     uiThread { toast("HTTP Failed") }
                     Log.e(TAG, e.message)
@@ -35,11 +49,41 @@ class MainActivity : AppCompatActivity() {
         httpRequestBisButton.setOnClickListener {
             doAsync {
                 try {
-                    val result = URL("http://flickr.com").readText()
-                    uiThread { toast("HTTP Bis OK") }
-                    Log.d(TAG, "result=$result")
+                    val request = Request.Builder().url("http://flickr.com").build()
+
+                    val response = okHttpClient.newCall(request).execute()
+
+                    if (response.isSuccessful) {
+                        uiThread { toast("HTTP Bis OK") }
+                        Log.d(TAG, "result=${response.body()?.string()}")
+                    } else {
+                        uiThread { toast("HTTP Bis Failed") }
+                        Log.w(TAG, "result=${response.message()}")
+                    }
                 } catch (e: Exception) {
                     uiThread { toast("HTTP Bis Failed") }
+                    Log.e(TAG, e.message)
+                }
+            }
+        }
+
+        val http2RequestButton: Button = findViewById(R.id.http2_request_button)
+        http2RequestButton.setOnClickListener {
+            doAsync {
+                try {
+                    val request = Request.Builder().url("http://flickr.com").addHeader("Upgrade", "h2c").addHeader("HTTP2-Settings", "VGhpcyBpcyBhIHRlc3Qh").build()
+
+                    val response = okHttpClient.newCall(request).execute()
+
+                    if (response.isSuccessful) {
+                        uiThread { toast("HTTP2 OK") }
+                        Log.d(TAG, "result=${response.body()?.string()}")
+                    } else {
+                        uiThread { toast("HTTP2 Failed") }
+                        Log.w(TAG, "result=${response.message()}")
+                    }
+                } catch (e: Exception) {
+                    uiThread { toast("HTTP2 Failed") }
                     Log.e(TAG, e.message)
                 }
             }
@@ -49,9 +93,17 @@ class MainActivity : AppCompatActivity() {
         httpsRequestButton.setOnClickListener {
             doAsync {
                 try {
-                    val result = URL("https://httpbin.org/html").readText()
-                    uiThread { toast("HTTPS OK") }
-                    Log.d(TAG, "result=$result")
+                    val request = Request.Builder().url("https://httpbin.org/html").build()
+
+                    val response = okHttpClient.newCall(request).execute()
+
+                    if (response.isSuccessful) {
+                        uiThread { toast("HTTPS OK") }
+                        Log.d(TAG, "result=${response.body()?.string()}")
+                    } else {
+                        uiThread { toast("HTTPS Failed") }
+                        Log.w(TAG, "result=${response.message()}")
+                    }
                 } catch (e: Exception) {
                     uiThread { toast("HTTPS Failed") }
                     Log.e(TAG, e.message)
@@ -63,13 +115,47 @@ class MainActivity : AppCompatActivity() {
         httpsRequestBisButton.setOnClickListener {
             doAsync {
                 try {
-                    val result = URL("https://flickr.com").readText()
-                    uiThread { toast("HTTPS Bis OK") }
-                    Log.d(TAG, "result=$result")
+                    val request = Request.Builder().url("https://flickr.com").build()
+
+                    val response = okHttpClient.newCall(request).execute()
+
+                    if (response.isSuccessful) {
+                        uiThread { toast("HTTPS Bis OK") }
+                        Log.d(TAG, "result=${response.body()?.string()}")
+                    } else {
+                        uiThread { toast("HTTPS Bis Failed") }
+                        Log.w(TAG, "result=${response.message()}")
+                    }
                 } catch (e: Exception) {
                     uiThread { toast("HTTPS Bis Failed") }
                     Log.e(TAG, e.message)
                 }
+            }
+        }
+
+        val otherTcpRequestButton : Button = findViewById(R.id.other_tcp_request_button)
+        otherTcpRequestButton.setOnClickListener {
+            doAsync {
+                val socket = Socket("193.51.236.75", 1234)
+                val out = PrintWriter(socket.getOutputStream(), true)
+                out.println("This is a TCP request test.")
+                out.close()
+                socket.close()
+                uiThread { toast("Other TCP OK") }
+            }
+        }
+
+        val otherUdpRequestButton : Button = findViewById(R.id.other_udp_request_button)
+        otherUdpRequestButton.setOnClickListener {
+            doAsync {
+                val datagramSocket = DatagramSocket()
+
+                val message = "This is a UDP request test."
+                val buffer = message.toByteArray()
+                val packet = DatagramPacket(buffer, buffer.size, InetAddress.getByName("193.51.236.75"), 4321)
+                packet.data = buffer
+                datagramSocket.send(packet)
+                uiThread { toast("Other UDP OK") }
             }
         }
     }
